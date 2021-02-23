cf1d715527329f309cab364d7e60771c8fe6be65
J.Laro <joselarojr@gmail.com>
2021-02-23 18:27:37 -0500

    initial push, structurng files/folders

---
+++ userdirectory/src/components/DataTable.jsx
@@ -0,0 +1,13 @@
+// import React from "react";
+
+// DataTable = () => {
+//     return (
+//         <div className="datatable mt-5">
+//             <table
+//                 id="table"
+//                 className="table table-striped table-hover table"
+//         </div>
+//     )
+// }
+
+// export default DataTable;
